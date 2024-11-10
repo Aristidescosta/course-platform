8634b7bd4d3dda5a481ab2a5dca1bdf54bdb39d7
Aristidescosta <aristidiscosta200@gmail.com>
2024-11-10 03:11:01 +0100

    feat: add props to card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,14 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <main className="bg-background">
-      <Card />
+      <Card
+        title="🏆 Curso de API Rest, Node e Typescript"
+        description={`
+          Curso de NodeJS e Typescript iniciante. Esse curso é focado nas principais conceitos de um backend para crud. Vamos trabalhar com paginação, filtro, banco de dados sql, query builders, typescript e muito mais. É a porta de entrada para o mundo do Node JS no backend.\n\n#CODARSE
+          `}
+        image="https://i.ytimg.com/vi/SVepTuBK4V0/hqdefault.jpg"
+        href="/cursos/123"
+      />
     </main>
   );
 }
