ffc0aba390f7838cdca7c5f6abac8c55fa4f997c
Aristidescosta <aristidiscosta200@gmail.com>
2024-11-11 13:52:38 +0100

    feat: Adding scroll controls to the section

--- src/components/section/index.tsx
+++ src/components/section/index.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
+"use client";
+
+import React, { useRef } from 'react';
 import { Card, ICardProps } from '../card';
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from 'react-icons/md';
 
 interface ISectionProps {
     title: string;
@@ -7,10 +10,32 @@ interface ISectionProps {
     variant: "grid" | "h-list"
 }
 export const Section = ({ title, variant = "grid", items }: ISectionProps) => {
+
+    const listRef = useRef<HTMLUListElement>(null);
+
+    const scrollToPosition = (position: number) => {
+        const currentScrollPosition = listRef.current?.scrollLeft || 0;
+        listRef.current?.scrollTo({
+            behavior: "smooth",
+            left: position + currentScrollPosition,
+            top: 0,
+        });
+    };
+
+
     return (
         <section className='flex flex-col gap-4 px-4'>
             <h2 className='font-bold text-xl'>{title}</h2>
-            <ul data-variant={variant} className='grid grid-cols-1 sm:grid-cols-none data-[variant=grid]:sm:grid-cols-2 data-[variant=grid]:md:grid-cols-3 gap-2 data-[variant=h-list]:sm:grid-flow-col data-[variant=h-list]:sm:overflow-x-auto'>
+            <ul
+                ref={listRef}
+                data-variant={variant}
+                className='grid grid-cols-1 sm:grid-cols-none data-[variant=grid]:sm:grid-cols-2 data-[variant=grid]:md:grid-cols-3 gap-2 data-[variant=h-list]:sm:grid-flow-col data-[variant=h-list]:sm:overflow-x-auto'
+            >
+
+                <button onClick={() => scrollToPosition(-300)} className='h-14 w-14 bg-primary rounded-full flex items-center justify-center sticky my-auto left-0 -ml-14'>
+                    <MdKeyboardArrowLeft size={32} />
+                </button>
+
                 {
                     items.map((item) => (
                         <li key={item.title} className='w-full data-[variant=h-list]:sm:w-72' data-variant={variant}>
@@ -18,16 +43,13 @@ export const Section = ({ title, variant = "grid", items }: ISectionProps) => {
                         </li>
                     ))
                 }
-                {/* <li className='w-full data-[variant=h-list]:sm:w-72' data-variant={variant}>
-                    <Card
-                        title="🏆 Curso de API Rest, Node e Typescript"
-                        description={`
-          Curso de NodeJS e Typescript iniciante. Esse curso é focado nas principais conceitos de um backend para crud. Vamos trabalhar com paginação, filtro, banco de dados sql, query builders, typescript e muito mais. É a porta de entrada para o mundo do Node JS no backend.\n\n#CODARSE
-          `}
-                        image="https://i.ytimg.com/vi/SVepTuBK4V0/hqdefault.jpg"
-                        href="/cursos/123"
-                    />
-                </li> */}
+
+                <button
+                    onClick={() => scrollToPosition(300)}
+                    className='h-14 w-14 bg-primary rounded-full flex items-center justify-center sticky my-auto right-0 -ml-14'
+                >
+                    <MdKeyboardArrowRight size={32} />
+                </button>
             </ul>
         </section>
     );
