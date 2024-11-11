ffd1236aeea7ee8a522ce9a59ed0c106a0852dcf
Aristidescosta <aristidiscosta200@gmail.com>
2024-11-11 14:43:12 +0100

    feat: improve section button and scroll

--- src/components/section/index.tsx
+++ src/components/section/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useRef } from 'react';
+import React, { UIEvent, useRef, useState } from 'react';
 import { Card, ICardProps } from '../card';
 import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from 'react-icons/md';
 
@@ -10,16 +10,27 @@ interface ISectionProps {
     variant: "grid" | "h-list"
 }
 export const Section = ({ title, variant = "grid", items }: ISectionProps) => {
-
     const listRef = useRef<HTMLUListElement>(null);
+    const [scrollAt, setScrollAt] = useState<"start" | "middle" | "end">("start");
 
     const scrollToPosition = (position: number) => {
-        const currentScrollPosition = listRef.current?.scrollLeft || 0;
+        const currentScrollLeft = listRef.current?.scrollLeft || 0;
         listRef.current?.scrollTo({
             behavior: "smooth",
-            left: position + currentScrollPosition,
+            left: position + currentScrollLeft,
             top: 0,
         });
+    };
+
+    const handleScroll = (e: UIEvent<HTMLUListElement, globalThis.UIEvent>) => {
+        const currentScrollLeft = e.currentTarget?.scrollLeft || 0;
+        const scrollBarWidth = e.currentTarget?.scrollWidth || 0;
+        const clientWidth = e.currentTarget?.clientWidth || 0;
+        if (currentScrollLeft === 0) {
+            setScrollAt("start");
+        } else if ((scrollBarWidth - clientWidth) === currentScrollLeft) {
+            setScrollAt("end");
+        } else setScrollAt("middle");
     };
 
 
@@ -28,13 +39,21 @@ export const Section = ({ title, variant = "grid", items }: ISectionProps) => {
             <h2 className='font-bold text-xl'>{title}</h2>
             <ul
                 ref={listRef}
+                onScroll={handleScroll}
                 data-variant={variant}
                 className='grid grid-cols-1 sm:grid-cols-none data-[variant=grid]:sm:grid-cols-2 data-[variant=grid]:md:grid-cols-3 gap-2 data-[variant=h-list]:sm:grid-flow-col data-[variant=h-list]:sm:overflow-x-auto'
             >
 
-                <button onClick={() => scrollToPosition(-300)} className='h-14 w-14 bg-primary rounded-full flex items-center justify-center sticky my-auto left-0 -ml-14'>
-                    <MdKeyboardArrowLeft size={32} />
-                </button>
+                {
+                    variant === "h-list" &&
+                    <button
+                        onClick={() => scrollToPosition(-300)}
+                        className='h-14 w-14 bg-primary rounded-full hidden sm:flex items-center transition-opacity justify-center sticky my-auto left-0 -ml-14 disabled:opacity-0 active:opacity-80'
+                        disabled={scrollAt === "start"}
+                    >
+                        <MdKeyboardArrowLeft size={32} />
+                    </button>
+                }
 
                 {
                     items.map((item) => (
@@ -44,12 +63,16 @@ export const Section = ({ title, variant = "grid", items }: ISectionProps) => {
                     ))
                 }
 
-                <button
-                    onClick={() => scrollToPosition(300)}
-                    className='h-14 w-14 bg-primary rounded-full flex items-center justify-center sticky my-auto right-0 -ml-14'
-                >
-                    <MdKeyboardArrowRight size={32} />
-                </button>
+                {
+                    variant === "h-list" &&
+                    <button
+                        onClick={() => scrollToPosition(300)}
+                        className='h-14 w-14 bg-primary rounded-full hidden sm:flex items-center transition-opacity justify-center sticky my-auto right-0 -ml-14 disabled:opacity-0 active:opacity-80'
+                        disabled={scrollAt === "end"}
+                    >
+                        <MdKeyboardArrowRight size={32} />
+                    </button>
+                }
             </ul>
         </section>
     );
